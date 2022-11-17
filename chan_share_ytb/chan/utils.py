d8d249263943a1e2987b54c619d8ca8e60274b81
SLeRest <sebastienlerest22@gmail.com>
2022-11-17 04:07:50 +0100

    add song, dispach all code in multiple functions in utils

--- chan_share_ytb/chan/utils.py
+++ chan_share_ytb/chan/utils.py
@@ -1,9 +1,19 @@
-import youtube_dl
 from django.conf import settings
+from django.core.files import File
+
+import requests
+import os
+import shutil
+import youtube_dl
+from datetime import timedelta, datetime
+from pathlib import Path
+from PIL import Image
 
 def download_ytb_mp3(url, id):
+    # TODO automatic log sur stdout dans youtubeDL
+    # voir comment eviter ca
     video_info = youtube_dl.YoutubeDL().extract_info(
-        url = url,download=False
+        url=url, download=False
     )
     filename = f"{settings.PATH_SONGS}/{id}.mp3"
     options={
@@ -13,3 +23,48 @@ def download_ytb_mp3(url, id):
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
+
+def get_thumnnail_png(song, video_info):
+    # Download thumbnail and put it in thumbnail dir
+    r = requests.get(video_info['thumbnail'], stream = True)
+    webp_path = f'{settings.DATA_THUMBNAIL_PATH}/{video_info["id"]}.webp'
+    png_path = f'{settings.DATA_THUMBNAIL_PATH}/{video_info["id"]}.png'
+    if r.status_code == 200:
+        try:
+            with open(webp_path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        except Exception as e:
+            raise e
+    else:
+        raise APIException(r.text)
+    # Convert webp file to png file
+    try:
+        with Image.open(webp_path).convert("RGB") as im:
+            im.save(png_path, "png")
+    except Exception as e:
+        raise APIException(r.text)
+    # Delete webp Image
+    try:
+        os.remove(webp_path)
+    except OSError as e:  ## if failed, report it back to the user ##
+        print (f'Error: {e.filename} - {e.strerror}')
+    # Add image to BDD
+    with Path(png_path).open(mode='rb') as f:
+        song.thumbnail = File(f, name=Path(png_path).name)
+        song.save()
+    return song
+
+def create_song_from_video_info(song, video_info):
+    song.id_ytb = video_info["id"]
+    song.title = video_info["title"]
+    song.channel_id = video_info["channel_id"]
+    song.channel_url = video_info["channel_url"]
+    song.channel_title = video_info["channel"]
+    song.uploader_id = video_info["uploader_id"]
+    song.uploader_name = video_info["uploader"]
+    song.duration = timedelta(seconds=video_info["duration"])
+    song.description = video_info["description"]
+    song.upload_date = datetime.strptime(video_info["upload_date"], '%Y%m%d').date()
+    for tag in video_info["tags"] : song.tags.add(tag) 
+    song = get_thumnnail_png(song, video_info)
+    return song
--- chan_share_ytb/chan/views.py
+++ chan_share_ytb/chan/views.py
@@ -1,12 +1,3 @@
-import sys
-import logging
-import requests
-import shutil
-from pathlib import Path
-from datetime import timedelta, datetime
-
-from PIL import Image
-
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.conf import settings
@@ -15,11 +6,12 @@ from django.core.files import File
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from chan.models import Song 
-from chan.permissions import SongPermission
+#from chan.permissions import SongPermission
 from chan.serializers import SongSerializer, SongCreateSerializer
-from chan.utils import download_ytb_mp3
+from chan.utils import download_ytb_mp3, create_song_from_video_info
 
 import youtube_dl
 # Create your views here.
@@ -28,7 +20,10 @@ import youtube_dl
 class SongViewset(ModelViewSet):
 
     serializer_class = SongSerializer
-    permission_classes = [SongPermission]
+    # TODO faire reflechir aux droit des song (tres lier aux droits des playlist) 
+    #permission_classes = [SongPermission]
+     # En phase de dev, on va pas se prendre la tete avec la permission
+    permission_classes = [AllowAny]
     # permission_classes = [
     #         IsAuthenticated, # TODO Check si user authentifier
     #         IsOwnerSong, # TODO check si c'est le creater / owner du son
@@ -55,103 +50,41 @@ class SongViewset(ModelViewSet):
     # TODO
     # la on fait des print pour chaque erreur car on est en dev mode mais
     # faudrait une table dans la bdd pour les logs error
+    # TODO faut split cette fonction en plusieur c'est inbuvable
     def create(self, request):
         # Check si le son est deja present dans la BDD
         url_ytb = request.data["url_ytb"]
         song = Song.objects.filter(url_ytb=url_ytb)
         if song.count() > 0:
             # Le son est present
+            # TODO check si le status du download du son
+            # si le status est pas done faut re-try de dl le son
             serializer = SongSerializer(song.first())
             return Response(serializer.data)
         # si il n'est pas present on va l'ajouter
         song = Song(url_ytb=request.data["url_ytb"])
         song.save()
-        print(f'Song saved: {song.title}')
 
         # On test le download
         try:
             video_info = youtube_dl.YoutubeDL().extract_info(url=url_ytb, download=False)
-            print("Video data Ok")
         except Exception as e:
             # TODO raise des exception specifique
             # pas uniquement 404, youtube peut etre down par exemple
             raise APIException(str(e))
+        song = create_song_from_video_info(song, video_info)
 
-        song.id_ytb = video_info["id"]
-        song.title = video_info["title"]
-        song.channel_id = video_info["channel_id"]
-        song.channel_url = video_info["channel_url"]
-        song.channel_title = video_info["channel"]
-        song.uploader_id = video_info["uploader_id"]
-        song.uploader_name = video_info["uploader"]
-        song.duration = timedelta(seconds=video_info["duration"])
-        song.description = video_info["description"]
-        song.upload_date = datetime.strptime(video_info["upload_date"], '%Y%m%d').date()
-        for tag in video_info["tags"] : song.tags.add(tag) 
+        # TODO NEXT VERSION
+        # add Song to queue
+        # la queue sera un autre programme dans un autre process python
+        # dans le meme container docker
+        # la queue se chargera de DL les son qui se trouve dans la file d'attente
+        # et de changer le status_download dans la BDD
 
-        # Download thumbnail and put it in songs_data dir
-        r = requests.get(video_info['thumbnail'], stream = True)
-        if r.status_code == 200:
-            p = f'{settings.DATA_SONG_PATH}/{video_info["id"]}.webp'
-            with open(p, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-            print("Image downloaded")
-        else:
-            print(f'Failed download: {r.status_code}: {r.text}')
-            raise APIException(r.text)
+        # TODO
+        # change status in Download
+        # download
+        # change status in Done
 
-        # Convert webp file to png file
-        try:
-            with Image.open(p).convert("RGB") as im:
-                p = f'{settings.DATA_SONG_PATH}/{video_info["id"]}.png'
-            im.save(p, "png")
-            print(f'Image converted to png: {p}')
-        except Exception as e:
-            print(f'Error convert webp to png: {str(e)}')
-            raise APIException(r.text)
-
-        # Add image to BDD
-        with Path(p).open(mode='rb') as f:
-            song.thumbnail = File(f, name=Path(p).name)
-            song.save()
         serializer = SongSerializer(song)
         return Response(serializer.data)
-
-    # @action(detail=True, methods=['post'])
-    # def download(self, request, id):
-    #
-    # @action(detail=True, methods=['get'])
-    # def status(self, request, id):
-    #     s = Song.objects.get(id=id)
-    #     return Response(s.download_status)
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     A viewset that provides the standard actions
-#     """
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-#     @action(detail=True, methods=['post'])
-#     def set_password(self, request, pk=None):
-#         user = self.get_object()
-#         serializer = PasswordSerializer(data=request.data)
-#         if serializer.is_valid():
-#             user.set_password(serializer.validated_data['password'])
-#             user.save()
-#             return Response({'status': 'password set'})
-#         else:
-#             return Response(serializer.errors,
-#                     status=status.HTTP_400_BAD_REQUEST)
-#
-#     @action(detail=False)
-#     def recent_users(self, request):
-#         recent_users = User.objects.all().order_by('-last_login')
-#
-#         page = self.paginate_queryset(recent_users)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-#
-#         serializer = self.get_serializer(recent_users, many=True)
-#         return Response(serializer.data)
